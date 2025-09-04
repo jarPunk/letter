f994497823b1d9b6ea01878519889f96ed5b5ddd
jarPunk <jar_ax@hotmail.com>
2025-09-03 23:48:12 -0400

    se elimino lo que no es necesario

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 $(document).ready(function(){
-      // Al cargar la página, ocultamos las cortinas
-      $('.left-curtain').css('width', '0%');
-      $('.right-curtain').css('width', '0%');
-    
-      $('.valentines-day').click(function(){
-        const $v = $(this);
+  $('.left-curtain').css('width', '0%');
+  $('.right-curtain').css('width', '0%');
 
-        // Primer click: flip (si aún no se ha volteado)
-        if (!$v.hasClass('flipped') && !$v.hasClass('flipping')) {
-          $v.addClass('flipping');
-          setTimeout(function(){
-            $v.removeClass('flipping').addClass('flipped');
-            // $v.find('.behind').hide();  <-- eliminado para mantener 3D
-          }, 800); // coincide con duración del giro
-          return;
-        }
+  $('.valentines-day').click(function(){
+    const $v = $(this);
 
-        // Segundo click: abrir (mostrar frente abierto)
-        if ($v.hasClass('flipped') && !$v.hasClass('open')) {
-          $v.addClass('open');
-          return;
-        }
-        
-      });
-    });
+    if (!$v.hasClass('flipped') && !$v.hasClass('flipping')) {
+      $v.addClass('flipping');
+      setTimeout(function(){
+        $v.removeClass('flipping').addClass('flipped');
+      }, 800);
+      return;
+    }
+
+    if ($v.hasClass('flipped') && !$v.hasClass('open')) {
+      $v.addClass('open');
+      return;
+    }
+  });
+});
